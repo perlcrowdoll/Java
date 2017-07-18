06a1971c10a25e27413ca222db80469b6be0574f
ylavitskas <ylavitskas@559OPT7010-1.sibfl.net>
2017-07-18 10:34:26 -0400

    Done

--- Framed/src/yaroslav/lavitskas/framed/Framed.java
+++ Framed/src/yaroslav/lavitskas/framed/Framed.java
@@ -3,8 +3,10 @@ package yaroslav.lavitskas.framed;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.util.Random;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
@@ -40,7 +42,8 @@ public class Framed extends JFrame {
 					LightButton button = (LightButton) e.getSource();
 					int row2 = button.getRow();
 					int col2 = button.getCol();
-					toggleLight(row2, col2);
+					toggleLights(row2, col2);
+					endGameIfDone();
 				});
 				centerPanel.add(lightButton[row][col]);
 			}
@@ -51,10 +54,127 @@ public class Framed extends JFrame {
 		setLocationRelativeTo(null);		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
+		newGame();
+		
+	}
+	private void endGameIfDone()
+	{
+		boolean done = 
+		   lightButton[0][0].isLit()
+		&& lightButton[0][1].isLit()
+		&& lightButton[0][2].isLit()
+		&& lightButton[1][0].isLit()
+		&& !lightButton[1][1].isLit()
+		&& lightButton[1][2].isLit()
+		&& lightButton[2][0].isLit()
+		&& lightButton[2][1].isLit()
+		&& lightButton[2][2].isLit();
+		String message = "Congratulations! You won! Do you want to play again?";
+		if (done)
+		{
+			int option = JOptionPane.showConfirmDialog(this, message, "Play again?", JOptionPane.YES_NO_OPTION);
+			if (option == JOptionPane.YES_OPTION) newGame();
+			else System.exit(0);
+		}
+		
+		
 	}
 	
-	private  void toggleLight(int row, int col)
+	private void newGame()
 	{
+		for (int row=0; row<GRIDSIZE; row++)
+		{
+			for (int col=0; col<GRIDSIZE; col++)
+			{
+				lightButton[row][col].toggle();
+			}
+			lightButton[1][1].toggle();
+		}
+		
+		Random rand = new Random();
+		for (int i=0;i<10;i++)
+		{
+		int numberOfTimes = rand.nextInt(10);
+		int row = rand.nextInt(GRIDSIZE);
+		int col = rand.nextInt(GRIDSIZE);
+		toggleLights(row, col);		
+		}
+	}
+	
+	private  void toggleLights(int row, int col)
+	{
+		//top left corner
+		if (row==0 && col==0)
+		{
+			lightButton[1][1].toggle();
+			lightButton[0][1].toggle();
+			lightButton[1][0].toggle();
+		}
+		
+		//top right corner
+		if (row==0 && col==2)
+		{
+			lightButton[0][1].toggle();
+			lightButton[1][1].toggle();
+			lightButton[1][2].toggle();
+		}
+		
+		//bottom left corner
+		if (row==2 && col==0)
+		{
+			lightButton[1][0].toggle();
+			lightButton[1][1].toggle();
+			lightButton[2][1].toggle();
+		}
+		
+		//bottom right corner
+		if (row==2 && col==2)
+		{
+			lightButton[1][2].toggle();
+			lightButton[2][1].toggle();
+			lightButton[1][1].toggle();
+		}
+		
+		
+		//top row middle
+		if (row==0 && col==1)
+		{
+			lightButton[0][0].toggle();
+			lightButton[0][2].toggle();
+		}
+		
+		//bottom row middle
+		if (row==2 && col==1)
+		{
+			lightButton[2][0].toggle();
+			lightButton[2][2].toggle();
+		}
+		
+		//left side middle
+		if (row==1 && col==0)
+		{
+			lightButton[0][0].toggle();
+			lightButton[2][0].toggle();
+		}
+		
+		//right side middle
+		if (row==1 && col==2)
+		{
+			lightButton[0][2].toggle();
+			lightButton[2][2].toggle();
+		}
+		
+		// center
+		if (row==1 && col==1)
+		{
+			lightButton[0][1].toggle();
+			lightButton[2][1].toggle();
+			lightButton[1][0].toggle();
+			lightButton[1][2].toggle();
+		}
+		
+		
+		
 		lightButton[row][col].toggle();
 	}
 
