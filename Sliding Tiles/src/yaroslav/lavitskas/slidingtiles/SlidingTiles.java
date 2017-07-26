40edcdd0e576c95036e96c499fbeaf3a988b61dc
ylavitskas <ylavitskas@559OPT7010-1.sibfl.net>
2017-07-26 16:37:19 -0400

    update

--- Sliding Tiles/src/yaroslav/lavitskas/slidingtiles/SlidingTiles.java
+++ Sliding Tiles/src/yaroslav/lavitskas/slidingtiles/SlidingTiles.java
@@ -2,13 +2,16 @@ package yaroslav.lavitskas.slidingtiles;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 import yaroslav.lavitskas.mycomponents.TitleLabel;
@@ -19,11 +22,13 @@ public class SlidingTiles extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private static final String FILENAME = "slidingTilesImage.jpg";
+	private static final String FILENAME = "C:\\Users\\ylavitskas\\Downloads\\MoreDIYJava\\slidingTilesImage.jpg";
 	
 	private int tileSize=50;
 	private int gridSize=4;
 	private BufferedImage image = null;
+	private TileButton[][] tile = new TileButton[gridSize][gridSize];
+	private JPanel centerPanel = new JPanel();
 	
 	public SlidingTiles()
 	{
@@ -51,8 +56,35 @@ public class SlidingTiles extends JFrame {
 		TitleLabel titleLable = new TitleLabel("Sliding Tiles");
 		add(titleLable, BorderLayout.PAGE_START);
 		
+		//Main Panel
+		divideImage();
+		
+		//Button Panel
+		
 	}
 	
+
+	private void divideImage() {
+		centerPanel.setLayout(new GridLayout(gridSize, gridSize));
+		add(centerPanel, BorderLayout.CENTER);
+		centerPanel.removeAll();		
+		int imageId = 0;
+		for (int row=0; row<gridSize; row++)
+		{
+			for (int col=0; col<gridSize; col++)
+			{
+				int x =col*tileSize;
+				int y =row*tileSize;
+				BufferedImage subimage = image.getSubimage(x, y, tileSize, tileSize);
+				ImageIcon imageIcon = new ImageIcon(subimage);
+				tile[row][col] = new TileButton(imageIcon, imageId, row, col);
+				centerPanel.add(tile[row][col]);
+				imageId++;
+			}
+		}
+		centerPanel.revalidate();
+		
+	}
 
 	public static void main(String[] args) {
 		try {
